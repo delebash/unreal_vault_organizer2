26d908a0362c8da3da43690d5ee63a983358d786
delebash <danelebash@hotmail.com>
2025-12-10 22:49:09 -0500

    sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,19 @@
 <template>
-    <v-app>
-      <v-app-bar>header</v-app-bar>
-      <div class="d-flex flex-1-1-100">
-        <Sidebar />
-        <v-main class="pl-0 bg-grey" >
-          <router-view></router-view>
-        </v-main>
-      </div>
-      <v-footer border="t">footer</v-footer>
-    </v-app>
+  <v-app>
+    <v-app-bar
+        height="72"
+        flat
+    >
+      header
+    </v-app-bar>
+    <div class="d-flex flex-1-1-100">
+      <Sidebar/>
+      <v-main class="pl-0 bg-grey">
+        <router-view></router-view>
+      </v-main>
+    </div>
+    <v-footer border="t">footer</v-footer>
+  </v-app>
 </template>
 
 <script setup>
