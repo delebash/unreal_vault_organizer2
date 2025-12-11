b46ace07a168b5585b15a373e62a6bfceb6aab2b
delebash <danelebash@hotmail.com>
2025-12-10 22:43:58 -0500

    sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,16 @@
-<script setup>
-import { invoke } from "@tauri-apps/api/core";
-import { ref} from 'vue';
-import AppFooter from "@/components/AppFooter.vue";
-
-const drawer=ref(false);
-
-</script>
 <template>
-  <v-app>
-    <v-app-bar app>
-      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-      <v-toolbar-title>My Application</v-toolbar-title>
-    </v-app-bar>
-
-    <v-navigation-drawer permanent>
-      <v-list nav dense>
-        <!-- Navigation items go here -->
-      </v-list>
-    </v-navigation-drawer>
-
-    <v-main>
-      <router-view></router-view>
-    </v-main>
-    <AppFooter  />
-  </v-app>
-
+    <v-app>
+      <v-app-bar>header</v-app-bar>
+      <div class="d-flex flex-1-1-100">
+        <Sidebar />
+        <v-main class="pl-0 bg-grey" >
+          <router-view></router-view>
+        </v-main>
+      </div>
+      <v-footer border="t">footer</v-footer>
+    </v-app>
 </template>
-<style>
-</style>
+
+<script setup>
+import Sidebar from '@/components/SideBar.vue'
+</script>
---
+++ src/components/SideBar.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-navigation-drawer
+      border="r"
+      permanent
+      class="position-sticky"
+      :style="{maxHeight: height}"
+  >
+    <div style="border: 2px solid pink; height: 100%">nav bar</div>
+  </v-navigation-drawer>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+import { useLayout } from 'vuetify'
+
+const { mainStyles } = useLayout()
+const height = computed(
+    () => `calc(100vh - ${mainStyles.value['--v-layout-top']})`
+)
+</script>
